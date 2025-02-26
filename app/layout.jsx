d6e13bf9b6723c31e1b8743181c9f947654a533f
Bhuvan A R <155255254+Bhuvan-A-R@users.noreply.github.com>
2025-02-26 19:57:44 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,8 +24,6 @@ export default function RootLayout({ children }) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
       </head>
       <body className={outfit.className}>
         <ThemeProvider attribute="class" defaultTheme="light">
